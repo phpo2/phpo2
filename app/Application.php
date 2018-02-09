ac1678acabebbc3ed11adc09e40def16e35de98f
Nadeen Gamage <nadeengamage@Nadeens-MacBook-Pro.local>
2018-02-09 12:10:09 +0530

    load the composer for dependency manage

---
+++ app/Application.php
@@ -0,0 +1,9 @@
+<?php 
+
+/**
+ *
+ * Load the composer.
+ *
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 /**
  *
- * Create The Application
+ * Create The Application.
  *
  * First, we need to create an application instance. This creates an instance 
  * of the application ready to manipulate the services.
